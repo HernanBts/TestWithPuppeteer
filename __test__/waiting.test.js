a0cf9cc064fadf93617d109eb7b54d964a8d874a
HernanBts <bort05@hotmail.com>
2022-04-20 11:45:28 -0300

    more waiting functions

--- __test__/waiting.test.js
+++ __test__/waiting.test.js
@@ -14,15 +14,23 @@ xdescribe('My third test...', () => {
         // explicit waiting
         await page.waitForTimeout(1000)
         // selector waiting
-        await page.waitForSelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
+        await page.waitForSelector('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b')
         // xpath waiting
-        await page.waitForXPath('//*[@id="hplogo"]')
+        await page.waitForXPath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
 
         await page.goto('https://demoqa.com/modal-dialogs', {waitUntil: 'networkidle0'})
         const modalBtn = await page.waitForXPath('//*[@id="showSmallModal"]')
         await modalBtn.click()
         await page.waitForTimeout(1000)
         await page.click('#closeSmallModal', {waitForTimeout: 1000})
+
+        // function waiting
+        await page.waitForFunction(() => document.querySelector('#example-modal-sizes-title-sm').innerText === 'Small Modal')
+
+        const observeResize = page.waitForFunction('window.innerWidth < 100')
+        await page.setViewport({width: 90, height: 90})
+        await page.waitForTimeout(1000)
+        await observeResize
 
         await browser.close()
     }, 30000)
