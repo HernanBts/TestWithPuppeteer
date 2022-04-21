704b7b480bf03b23928ee47a74a97462f05d8143
HernanBts <bort05@hotmail.com>
2022-04-20 21:14:38 -0300

    geting data from pages

---
+++ __test__/extractInfo.test.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer')
+
+describe('Tests of extract info...', () => {
+    it('extracting value of button.', async() => {
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+        })
+        const page = await browser.newPage()
+        await page.goto('https://demoqa.com/buttons')
+
+        // functions and using selector
+        const title = await page.title()
+        const url = await page.url()
+        await page.waitForSelector('#rightClickBtn')
+        const valBtn = await page.$eval('#rightClickBtn', (button)=>button.textContent)
+
+        // using XPath
+        const button = await page.waitForXPath('//*[@id="rightClickBtn"]')
+        // const [button] = await page.$x('//*[@id="rightClickBtn"]')
+
+        const xValBtn = await page.evaluate((text) => text.textContent, button)
+
+        // obtain data
+        const data = await page.$$eval('img', (img) => img.length)
+
+        console.log(`#### Title #### ${title}
+                     \n#### URL #### ${url}
+                     \n#### XPath #### ${xValBtn}
+                     \n#### Value #### ${valBtn}
+                     \n#### Data #### ${data}`)
+
+        await browser.close()
+    }, 20000)
+})
